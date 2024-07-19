2a96ba409114c047e623c64ac2d89c9506bd24f4
Divyesh <35572841+raxlink@users.noreply.github.com>
2024-07-19 20:32:43 +0530

    Create Screen Buffer.

--- cmd_FPS/main.cpp
+++ cmd_FPS/main.cpp
@@ -2,8 +2,18 @@
 
 using namespace std;
 
+#include <windows.h>
+
+int nScreenWidth = 120;
+int nScreenHeight = 40;
+
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    // Create Screen Buffer
+    wchar_t *screen = new wchar_t[nScreenHeight * nScreenWidth];
+    HANDLE hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE,0,NULL, CONSOLE_TEXTMODE_BUFFER,NULL);
+    SetConsoleActiveScreenBuffer(hConsole);
+    DWORD dwByteWritten = 0;
     return 0;
 }
